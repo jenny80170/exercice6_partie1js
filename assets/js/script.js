ae695706843174227f20c60e2d6eb77f3e575b4a
jenny80170 <jennifer.malleret@outlook.fr>
2019-10-24 09:35:50 +0200

    exercice 6 partie 1 js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function trash() {
+  var number1 = document.getElementById('firstNumber').value;
+  var number2 = document.getElementById('secondNumber').value;
+  window.alert(number1 % number2);
+}
